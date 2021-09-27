f28c11b07e5af88a25c434d4c78314cd642dfe01
tmoran18 <moran.tim01@gmail.com>
2021-09-27 20:59:30 +1000

    add head in

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
+import Head from 'next/head'
 import Hero from '../components/Hero'
 import Header from '../components/Header'
 import About from '../components/About'
@@ -12,22 +12,10 @@ import Footer from '../components/Footer'
 import dynamic from 'next/dynamic'
 import Skills from '../components/Skills'
 import { Box, Button } from '@chakra-ui/react'
-=======
-import Head from "next/head";
-import Hero from "../components/Hero";
-import Header from "../components/Header";
-import About from "../components/About";
-import Work from "../components/Work";
-import Banner from "../components/Banner";
-import Services from "../components/Services";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
-import dynamic from "next/dynamic";
->>>>>>> 748e0dc9a6d6dd9d88227867949ea2e2437c8d0d
 
-const DynamicNavbar = dynamic(() => import("../components/Navbar"), {
+const DynamicNavbar = dynamic(() => import('../components/Navbar'), {
   ssr: false,
-});
+})
 
 export default function Home() {
   return (
@@ -35,34 +23,34 @@ export default function Home() {
       <Head>
         <title>Tim Moran - Frontend Developer Brisbane</title>
         <meta
-          name="description"
-          content="I am a Brisbane based web developer with a passion for producing custom websites &amp; applications for small to medium businesses. ReactJS, HTML, CSS, JavaScript, NextJS"
+          name='description'
+          content='I am a Brisbane based web developer with a passion for producing custom websites &amp; applications for small to medium businesses. ReactJS, HTML, CSS, JavaScript, NextJS'
         ></meta>
         <meta
-          name="keywords"
-          content="freelance web developer, website developer, web developer brisbane, react developer, nextJS developer, static site generator developer, website developer brisbane"
+          name='keywords'
+          content='freelance web developer, website developer, web developer brisbane, react developer, nextJS developer, static site generator developer, website developer brisbane'
         ></meta>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
         />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff"></meta>
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff'></meta>
       </Head>
       <DynamicNavbar />
       <Hero />
@@ -78,5 +66,5 @@ export default function Home() {
       <Contact />
       <Footer />
     </>
-  );
+  )
 }
